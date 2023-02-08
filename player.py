8e0ef1252419f902ae8e0fb9c001f638eb913279
Nicola Slater-Arnold <nicolaslaterarnold@gmail.com>
2023-02-08 09:05:54 +0000

    Minor tweaks to player

--- player.py
+++ player.py
@@ -1,7 +1,16 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+# TODO Create a turtle player that starts at the bottom of the screen and listen for the 
+#  "Up" keypress to move the turtle north
+
+class Player(Turtle):
+    # These two lines of code simple mean that the player class can do everything that the turtle class can do, but
+    # we can now add more to it. You can get the super call using the lightbulb!
+    def __init__(self):
+        super().__init__()
+
